0c0e7a4363f778a48408df1100fdfe12347b45cb
xandekk <alexandre.dosa@gmail.com>
2023-02-12 14:07:51 -0300

    add class select-none and remove kebab

--- app/javascripts/file_manager.js
+++ app/javascripts/file_manager.js
@@ -48,7 +48,6 @@ class FileManager {
  				button.innerHTML = '<svg class="w-12 h-12 mx-auto" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"/><path d="M21 9v11.993A1 1 0 0 1 20.007 22H3.993A.993.993 0 0 1 3 21.008V2.992C3 2.455 3.447 2 3.998 2H14v6a1 1 0 0 0 1 1h6zm0-2h-5V2.003L21 7z"/></svg>';
  			}
 
- 			button.addEventListener('click', this.showProperty.bind(this, file['name']));
  			button.className = 'rounded p-1 hover:bg-zinc-700 focus:bg-zinc-600 focus:outline-none focus:ring-2 focus:ring-zinc-500'
 			p.className = 'break-all';
 			p.textContent = name.split('/').at(-1);
